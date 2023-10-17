b67825da5a085791c818db2dfb3daeee906d7af7
kuttu101 <sandhra101@gmail.com>
2023-10-17 15:50:10 +0530

    add

--- python qns/banking.py
+++ python qns/banking.py
@@ -1,9 +1,10 @@
 bank={}
-account_num=1234
+account_no=1234
 balance=0
 details=[]
+acc=[]
 while True:
-    print("1.create account","2.view account","3.exit")
+    print("1.create account","2.view account","3.add balance","4.withdraw balance","5.exit")
     choice=int(input('enter your choice'))
     if choice==1:
         name=input('enter your name')
@@ -12,12 +13,37 @@ while True:
         bank["age:"]=age
         contact=int(input('enter your number'))
         bank["contact:"]=contact
-        bank["account number:"]=account_num
-        account_num=account_num+1
+        bank["account number:"]=account_no
+        account_no=account_no+1
         bank["balance:"]=balance
-    elif choice==2:
         details.append(bank.copy())
-        print(details)
-        
+    elif choice==2: 
+        print(details) 
     elif choice==3:
-        exit()
+        account_no=int(input("enter your account number"))
+        amount=int(input("enter the amount"))
+        for i in details:
+            acc.append(i['account number:'])
+        if account_no in acc:
+               for i in details:
+                   if i['account number:']==account_no:
+                      i['balance:']+=amount
+        else:
+                       print('account not accessible')
+    elif choice==4:
+         account_no=int(input("enter your account number"))
+         amount=int(input("enter the amount"))
+         for i in details:
+          acc.append(i["account number:"])
+        #  print(acc)
+         if account_no in acc:
+             for i in details:
+                 if i['account number:']==account_no:
+                     i['balance:']-=amount
+         else:
+             print("not possible")
+    elif choice==5:
+        exit()            
+
+    
+        
