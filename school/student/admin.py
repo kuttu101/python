f67323eb575abd999e6af84f82f0fb521eef0587
kuttu101 <sandhra101@gmail.com>
2023-12-13 14:53:48 +0530

    add

--- school/student/admin.py
+++ school/student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from models import *
 # Register your models here.
+class Department_display(admin.ModelAdmin):
+    list_display=['name']
+    admin.site.register(Department,Department_display)
+     
